9b625d90f889dd1818e52cc9776a987d50d3d6f8
Anne-Lie Bäck <a-l.back@outlook.com>
2019-10-29 19:37:59 +0100

    Events intro
    The introducing events in the users cottage

--- script1.js
+++ script1.js
@@ -1,31 +1,52 @@
 
 /* Globala variabler */
 
+/**
+ * @param {string} userText - The text that shows the storys progress
+ * @param {string} userInput - The text the user writes in the input-field
+ * @param {object} basket - Contains the items the user has in their basket.
+ * @param {number} healthPoints - The users HP
+ */
 let text = document.getElementById("userText")
 let input = document.getElementById("userInput")
 const button = document.getElementById("userButton")
 
+let basket = {}
+let userName
+let healthPoints = 20
 
-/* TUTORIAL HALP */
 
-function getUserInput() {
-    console.log(input.value)
-    text.innerHTML = 'Hej ' + input.value + 'luvan! Vad vill du göra nu?'
+/* --- GAME PLAY --- */
 
+function firstUserInput() {
+    userName = input.value + "luvan"
+    text.innerHTML += '<br>"' + userName + '! Har du glömt att du ska till mormor med lunch idag?! " <br> Du reser dig upp ur sängen och tänker :"Gaffel också" och slänger på dig dina kläder och springer ner. <br> <br> Väl där nere frågar din mor: "Vad tror du mormor vill ha att äta idag?"'
+    
     button.onclick = event1
+    
 }
 
  function event1(){
-     console.log('wohoo')
+     basket.food = input.value
 
-     text.innerHTML = input.value + ' låter som en dålig idé'
-/*     let userInput = input.value
-    let userText = document.getElementById('userText')
+     text.innerHTML += '<br> "Okej, jaja. Jag packar väl ner ' + basket.food + '. Vad tror du hon vill ha att dricka?'
 
-    userText = input.value + " låter som en dålig idé"
-    text.HTML = userText */
+     button.onclick = event2
+
+    
 
 }
+
+function event2(){
+    basket.drink = input.value
+
+    text.innerHTML += '<br>"Tror du verkligen att hon dricker ' + basket.drink + '? Jaja, hon dricker säkert det mesta. <br>' +
+                        userName + ' med ett HP på ' + healthPoints + ' ger sig ut i skogen med en korg med ' + basket.food + ' och ' + basket.drink + '. Målet är: mormors stuga!'
+
+    button.onclick = event3
+}
+
+
 
 
 
