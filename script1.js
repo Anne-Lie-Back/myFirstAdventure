c68bb189b1a2fbf870d7d47b1b11e987527fd7f3
Anne-Lie Bäck <a-l.back@outlook.com>
2019-10-31 14:12:39 +0100

    Finally, back to main story-line solved!

--- script1.js
+++ script1.js
@@ -9,6 +9,7 @@
  * @param {number} strength - Your strength point
  * @param {number} breadCrumbs - Number of excisting breadcrumbs through the game
  * @param {boolean} wolf - Stores if you told the wolf the truth or not
+ * @param {Number} breadCrumbsLeft - tells how many crumbs there's left to find and tells the programme where you are in the story/series of head-functions
  * 
  */
 let text = document.getElementById("userText")
@@ -20,13 +21,20 @@ let basket = []
 let userName
 let healthPoints = 20
 let userStrength = 1
-let breadCrumbles =  5
+let breadCrumbles = 5
 let wolf = true
+let breadCrumbsLeft = 5
 
 
-/* --- GAME PLAY --- */
+/**
+ *  --- GAME PLAY ---
+ */
 
 
+/**
+ * --- USER IS AT HOME ---
+ * First input from user
+ */
 function initialInput(){
     userName = input.value + "luvan"
     text.innerHTML += '<br>"' + userName + '! Har du glömt att du ska till mormor med lunch idag?! " <br> Du reser dig upp ur sängen och tänker :"Gaffel också" och slänger på dig dina kläder och springer ner. <br> <br> Väl där nere frågar din mor: "Vad tror du mormor vill ha att äta idag?"'
@@ -43,67 +51,75 @@ function initialInput(){
      button.onclick = event2
 }
 
+/**
+ * --- GOOD BYE MOM. 
+ * The beginning of the user's journey. 
+ * The user finds its first breadcrumb
+ */
 function event2(){
     basket.push(input.value)
 
     text.innerHTML += '<br>"Tror du verkligen att hon dricker ' + basket[1] + '? Jaja, hon dricker säkert det mesta. <br>' +
                         userName + ' med ett HP på ' + healthPoints + ' ger sig ut i skogen med en korg med ' + basket[0] + ' och ' 
                         + basket[1] + '. Målet är: mormors stuga!' + 
-                         '<br> <br> Du traskar ut i den gröna, frodiga skogen. Fåglar kvittrar och solens strålar skapar silar genom trädkronorna och skapar skuggspel över stigen. Plötsligt ser du en brödsmula! <br> Vill du plocka upp brödsmulan? J/N'
+                         '<br> <br> Du traskar ut i den gröna, frodiga skogen. Fåglar kvittrar och solens strålar som silar genom trädkronorna och skapar skuggspel över stigen.'
 
-    button.onclick = pickBreadCrumble
+    breadCrumbsLeft = 4
 
+    seeBreadCrumb()
 }
-
-/**
- * -- BREADCRUMB AND PUSHUP --
- * 
- * @param {string} story - The text that takes the user further in the gameplay after pickBreadCrumble and doPushUp
- */
-const story = 'Skogen bjuder på många spännande dofter, men inget doftar så gott som din mormors kanelbullar. Du börjar fantisera om alla de kanelbullar du hoppas få äta när du kommer fram till mormors stuga. Bredvid ett träd precis vid stigen ser du en svamp. Mormor gillar svamp. Plocka upp den? (J/N)'
-
-
 
 
 
 function event3(){
+
+    text.innerHTML += 'Skogen bjuder på många spännande dofter, men inget doftar så gott som din mormors kanelbullar. Du börjar fantisera om alla de kanelbullar du hoppas få äta när du kommer fram till mormors stuga. Bredvid ett träd precis vid stigen ser du en svamp. Mormor gillar svamp. Plocka upp den? (J/N)'
     
-    if (input.value === 'j'){
-            text.innerHTML += 'Den ser stor och svampig ut. Vill du äta den på stubben? (J/N)'
-            console.log('test svamp')
-            button.onclick = eatMushroom
-    }
-        
-    else if (input.value === 'n'){
-            text.innerHTML += 'Nej fy, även om din mormor gillar svamp så hatar du att ens vidröra dess svampiga yta. Du ryser till vid tanken och springer vidare innan tanken hinenr ikapp igen'
-            event4()
-    }
-
-    else{
-            text.innerHTML += wrongInput
-            button.onclick = event3
-        
-    }
-
-}
-
-function eatMushroom(){
+    button.onclick = pickUpMushroom
     
-    if (input.value === 'j'){
-            youDied()
-    }
+    function pickUpMushroom(){
+
+        if (input.value === 'j'){
+                text.innerHTML += 'Den ser stor och svampig ut. Vill du äta den på stubben? (J/N)'
+                console.log('test svamp')
+                button.onclick = eatMushroom
+
+                function eatMushroom(){
         
-    else if(input.value === 'n'){
-            text.innerHTML += 'Du lägger ner svampen i korgen bland ' + basket[0] + ' och ' + basket[1] + ' och beger dig vidare in i skogen.'
-            basket.push('svamp')
+                    if (input.value === 'j'){
+                            youDied()
+                    }
+                        
+                    else if(input.value === 'n'){
+                            text.innerHTML += 'Du lägger ner svampen i korgen bland ' + basket[0] + ' och ' + basket[1] + ' och beger dig vidare in i skogen.'
+                            basket.push('svamp')
+                            
+                            event4()
+                    }
+                
+                    else{
+                            text.innerHTML += wrongInput
+                    }
+                }
+        }
             
-            event4()
+        else if (input.value === 'n'){
+                text.innerHTML += 'Nej fy, även om din mormor gillar svamp så hatar du att ens vidröra dess svampiga yta. Du ryser till vid tanken och springer vidare innan tanken hinenr ikapp igen'
+                event4()
+        }
+
+        else{
+                text.innerHTML += wrongInput
+                button.onclick = event3       
+        }
     }
 
-    else{
-            text.innerHTML += wrongInput
-    }
 }
+
+/**
+ * --- WOLF DIALOG ---
+ * the user's dialog with the fluffy wolf
+ */
 
 function event4(){
     text.innerHTML += '<br><br> Skogen bjuder på många spännande ljud. Fåglar som kvittrar, vinden som viner genom trädtopparna, en buske som prasslar medan en varg stiger ut ur den... ... ... <br> EN BUSKE SOM PRASSLAR MEDAN EN VARG STIGER UR DEN?!' +
@@ -120,7 +136,7 @@ function event4(){
             case 's' || 'S':
                 text.innerHTML += 'Du bestämmer dig för att allt fluffigt går att lita på och berättar sanningen: <br> "Hon bor i andra sidan skogen, om du tar höger vid stigen"'
 
-                button.onclick = event5
+                seeBreadCrumb()
                 break
 
             case 'f' || 'F':
@@ -139,73 +155,112 @@ function event4(){
     function tellLie(){
         
         text.innerHTML += '<br> "'+ input.value + '" <br> "Oj, då har du en bit att gå. Bäst att låta dig traska vidare. Farväl ' + userName + '!"'
-        console.log(wolf)
-        button.onclick = event5
+        
+        breadCrumbsLeft = 3
+        seeBreadCrumb()
+
+
     }
 }
 
 function event5(){
-    console.log(wolf)
+    text.innerHTML += 'hello'
 }
 
-function pickBreadCrumble(){
+/**
+ * ---- BREADCRUMB LOOP ---
+ * functions played everytime the user sees a breadcrumb
+ */
+
+function seeBreadCrumb(){
+
+    text.innerHTML += '<br> Din färd mot mormors stuga fortsätter. På stigen ser du en brödsmula. Plocka upp brödsmulan? (J/N)'
+
+    button.onclick = pickBreadCrumb
+
+    function pickBreadCrumb(){
     
-    if (input.value === 'j'){
-        breadCrumbles = breadCrumbles - 1
-        text.innerHTML += '<br> Du böjer dig ner och lockar upp brödsmulan i din hand. Du undersöker den noggrant, dammar av de fåtal gruskorn som satt på det, och slänger sedan in smulan i munnen. Medan du ändå är där nere så funderar du på om du ska göra en armhävning. <br> Gör du en armhävning? J/N' 
-        button.onclick = doPushUp
+        if (input.value === 'j'){
+            breadCrumbles = breadCrumbles - 1
+            text.innerHTML += '<br> Du böjer dig ner och lockar upp brödsmulan i din hand. Du undersöker den noggrant, dammar av de fåtal gruskorn som satt på det, och slänger sedan in smulan i munnen. Medan du ändå är där nere så funderar du på om du ska göra en armhävning. <br> Gör du en armhävning? J/N' 
+            button.onclick = doPushUp
 
-        function doPushUp(){
-
-            if(input.value === 'j'){
-                userStrength = userStrength + 1
-                text.innerHTML += '<br> Du gör en armhävning och känner dig som en riktig spännis.' + story
-                button.onclick = event3
+            function doPushUp(){
+                console.log('Gör en armhävning')
+                switch(input.value){
+                case 'j':
+                    userStrength = userStrength + 1
+                    text.innerHTML += '<br> Du gör en armhävning och känner dig som en riktig spännis.'
+                    eventDirection()
+                    break
+                
+            
+                case 'n':
+                    text.innerHTML += '<br> Äsh, trams. Vem i sitt sinnes fulla bruk gör en armhävning mitt ute i skogen?'
+                    eventDirection()
+                    break
+                
+            
+                default:
+                    input.value += wrongInput
+                    button.onlick = doPushUp
+                }
             }
-        
-            else if (input.value === 'n'){
-                text.innerHTML += '<br> Äsh, trams. Vem i sitt sinnes fulla bruk gör en armhävning mitt ute i skogen?' + story
-                button.onclick = event3
-            }
-        
-            else{
-                input.value += wrongInput
-                button.onlick = doPushUp
-            }    
         }
-        
+
+        else if (input.value === 'n'){
+            text.innerHTML += '<br> Brödsmulan ser inte tillräckligt aptitlig ut. Du promenerar vidare.'
+            eventDirection()
+            
+        }
+
+        else{
+            text.innerHTML += wrongInput
+            button.onclick = pickBreadCrumble
+        }
     }
 
-    else if (input.value === 'n'){
-        text.innerHTML += '<br> Brödsmulan ser inte tillräckligt aptitlig ut. Du promenerar vidare.' + story
-        button.onclick = event3
-    }
+    function eventDirection(){
+        switch (breadCrumbsLeft){
 
-    else{
-        text.innerHTML += wrongInput
-        button.onclick = pickBreadCrumble
+            case 4:
+                button.onclick = event3
+                break
+
+            case 3:
+                button.onclick = event5
+                break
+
+            default:
+                console.log('Nu blev det fel')
+
+        }
     }
 }
+
+/**
+ * ----- DEAD ----
+ * Function played when user dies
+ */
 
 function youDied(){
+
     text.innerHTML += '<p class="dead"><br> YOU DIEDED!</p> <p> Vill du spela igen? (J/N)</p>'
     button.onclick = startOver
 
     function startOver(){
-    switch (input.value) {
-        case 'j':
-            text.innerHTML = 'En solig dag i augusti vaknar du av att din mamma ropar på dig. (Skriv ditt namn)'
-            button.onclick = initialInput
-            break
+        switch (input.value) {
+            case 'j':
+                text.innerHTML = 'En solig dag i augusti vaknar du av att din mamma ropar på dig. (Skriv ditt namn)'
+                button.onclick = initialInput
+                break
 
-        case 'n':
-            text.innerHTML = '<p> Så spelet var för svårt för dig? Det är okej. Du är välkommen tillbaka när du blivit lite smartare. </p>'
-            break
-        default:
-            text.innerHTML = wrongInput
-    }
+            case 'n':
+                text.innerHTML = '<p> Så spelet var för svårt för dig? Det är okej. Du är välkommen tillbaka när du blivit lite smartare. </p>'
+                break
+
+            default:
+                text.innerHTML = wrongInput
+        }
     }
 }
-
-
-
